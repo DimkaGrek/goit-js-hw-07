dd51847bbeac4ab86ca5a4f26d92283af4bd89e7
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2023-11-22 18:26:31 +0200

    solution for task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,38 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const ul = document.querySelector('.gallery');
+
+ul.addEventListener('click', (event) => {
+    event.preventDefault();
+    const urlBig = event.target.getAttribute('data-source');
+
+    const instance = basicLightbox.create(`
+    <img src="${urlBig}">
+`);
+
+    instance.show();
+
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            instance.close();
+        }
+    });
+});
+
+const imgArr = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+});
+ul.insertAdjacentHTML('beforeend', imgArr.join('\n'));
+
+// const galleryLink = document.querySelector('.gallery__link');
+
+// console.log(galleryItems);
