797ce792630a8bed10d099821b92a9d44091d5cd
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2023-11-23 08:22:52 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,35 +2,6 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const ul = document.querySelector('.gallery');
 
-ul.addEventListener('click', (event) => {
-    event.preventDefault();
-    if (event.target.nodeName === 'IMG') {
-        const urlBig = event.target.getAttribute('data-source');
-
-        const instance = basicLightbox.create(`<img src="${urlBig}">`, {
-            onShow: (instance) => {
-                window.addEventListener('keydown', (e) => {
-                    onEscClick(e, instance);
-                });
-            },
-            onClose: (instance) => {
-                window.removeEventListener('keydown', (e) => {
-                    onEscClick(e, instance);
-                });
-            },
-        });
-
-        instance.show();
-    }
-});
-
-function onEscClick(e, instance) {
-    if (e.key === 'Escape') {
-        instance.close();
-    }
-    return;
-}
-
 const imgArr = galleryItems.map(({ preview, original, description }) => {
     return `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
@@ -44,3 +15,31 @@ const imgArr = galleryItems.map(({ preview, original, description }) => {
   </li>`;
 });
 ul.insertAdjacentHTML('beforeend', imgArr.join('\n'));
+
+ul.addEventListener('click', onImgClick);
+const instance = basicLightbox.create(`<img src="">`, {
+    onShow: (instance) => {
+        window.addEventListener('keydown', onEscClick);
+    },
+    onClose: (instance) => {
+        window.removeEventListener('keydown', onEscClick);
+    },
+});
+
+function onImgClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    instance.element().querySelector('img').src =
+        event.target.getAttribute('data-source');
+
+    instance.show();
+}
+
+function onEscClick(e) {
+    if (e.key === 'Escape') {
+        instance.close();
+    }
+    return;
+}
